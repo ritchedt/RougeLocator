77ea2a28af8181624f4ccdc057387e99e2923555
Drew T. Ritcher <ritchedt@miamioh.edu>
2017-03-26 14:27:25 -0400

    first commit

---
+++ src/main/java/com/drew/RogueLocatorApplication.java
@@ -0,0 +1,12 @@
+package com.drew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RogueLocatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RogueLocatorApplication.class, args);
+	}
+}
